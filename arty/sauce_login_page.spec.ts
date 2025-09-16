077dac816e3cf91067255db7f1490b0d32c74ee2
Arty-Z <arty.zhuria@gmail.com>
2025-09-15 20:28:59 -0500

    Login test
    Happy path

---
+++ arty/sauce_login_page.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Testing Sauce login page', async ({ page }) => {
+     
+     await page.goto('https://www.saucedemo.com/');
+     let loginInput = page.locator('div[class="form_group"] [id="user-name"]');
+     await loginInput.type("secret_sauce")
+     await page.waitForTimeout(2_000)
+
+     let passwordInput = page.locator('div[class="form_group"] [id="password"]');
+     await passwordInput.type("secret_sauce")
+     await page.waitForTimeout(2_000)
+
+     let loginbutton = page.locator('div[id="login_button_container"] [id="login-button"]');
+     await loginbutton.press("Enter");
+     await page.waitForTimeout(2_000)
+
+     let pageHeader = await page.locator('span[class="title"]').innerText();
+    
+     let expectedHeader= "Products";
+
+    expect (pageHeader).toBe(expectedHeader);
+});
