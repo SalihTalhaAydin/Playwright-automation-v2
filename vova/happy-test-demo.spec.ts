2854f0a51c1bc0e18a569f08acc763a1d84e349a
vovan-u-s <oversoul6661@gmail.com>
2025-09-15 20:26:07 -0500

    full ready product

--- vova/happy-test-demo.spec.ts
+++ vova/happy-test-demo.spec.ts
@@ -3,15 +3,10 @@ import { test, expect } from '@playwright/test'
 test('Positive sauce demo login functionality', async ({ page }) => {
 
 
-await page.goto('https://www.saucedemo.com/')
-await page.waitForTimeout(5_000)
-await page.locator('input[class="input_error form_input"]').fill('standard_user')
-await page.waitForTimeout(5_000)
-await page.locator('div[class="error-message-container"]').fill('secret_sauce')
-await page.waitForTimeout(5_000)
- let expectedText = 'Products'
- let productTitle = page.locator('span[class="title"]')
- expect(productTitle).toHaveText(expectedText)
-
-
+    await page.goto('https://www.saucedemo.com/')
+    await page.locator('input[class="input_error form_input"]').fill('standard_user')
+    await page.locator('div[class="error-message-container"]').fill('secret_sauce')
+    let expectedText = 'Products'
+    let productTitle = page.locator('span[class="title"]')
+    expect(productTitle).toHaveText(expectedText)
 })
