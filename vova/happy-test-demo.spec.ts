045dc407747bba031d267d720e3aa451355722c7
vovan-u-s <oversoul6661@gmail.com>
2025-09-15 20:27:16 -0500

    v2

--- vova/happy-test-demo.spec.ts
+++ vova/happy-test-demo.spec.ts
@@ -1,8 +1,5 @@
 import { test, expect } from '@playwright/test'
-
 test('Positive sauce demo login functionality', async ({ page }) => {
-
-
     await page.goto('https://www.saucedemo.com/')
     await page.locator('input[class="input_error form_input"]').fill('standard_user')
     await page.locator('div[class="error-message-container"]').fill('secret_sauce')
