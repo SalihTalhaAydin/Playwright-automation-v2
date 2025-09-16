bef059e18c259a0de50bb8f2e1fadd09c900008e
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-15 21:24:37 -0500

    formatted

--- alina/negative-case.spec.ts
+++ alina/negative-case.spec.ts
@@ -1,14 +1,14 @@
-import { test, expect } from '@playwright/test'
-test('Negative sauce demo login functionality', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/')
-    await page.locator('input[id="user-name"]').fill('standard_user')
-    await page.locator('input[id="password"]').fill('')
-    await page.locator('input[id="login-button"]').click()
-    let expectedMessage = 'Epic sadface: username and passwords do not match in this service';
-    let errorTitle = page.locator('h3[data-test="error"]');
-    expect(errorTitle).toHaveText(expectedMessage)
-})
-
+import { test, expect } from "@playwright/test";
+test("Negative sauce demo login functionality", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.locator('input[id="user-name"]').fill("standard_user");
+  await page.locator('input[id="password"]').fill("");
+  await page.locator('input[id="login-button"]').click();
+  let expectedMessage =
+    "Epic sadface: username and passwords do not match in this service";
+  let errorTitle = page.locator('h3[data-test="error"]');
+  expect(errorTitle).toHaveText(expectedMessage);
+});
 
 test("Negative test 2 sauce demo login functionality", async ({ page }) => {
   await page.goto("https://www.saucedemo.com/");
@@ -18,8 +18,7 @@ test("Negative test 2 sauce demo login functionality", async ({ page }) => {
   let expectedMessage1 =
     "Epic sadface: username and passwords do not match in this service";
   let errorTitle2 = page.locator('h3[data-test="error"]');
-    expect(errorTitle2).toHaveText(expectedMessage1);
-    await page.waitForTimeout(5_000)
+  expect(errorTitle2).toHaveText(expectedMessage1);
 });
 
 test("Negative test 3 sauce demo login functionality", async ({ page }) => {
@@ -31,6 +30,4 @@ test("Negative test 3 sauce demo login functionality", async ({ page }) => {
     "Epic sadface: username and passwords do not match in this service";
   let errorTitle2 = page.locator('h3[data-test="error"]');
   expect(errorTitle2).toHaveText(expectedMessage1);
-  await page.waitForTimeout(5_000);
 });
-
