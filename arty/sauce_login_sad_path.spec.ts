170dd54e9240d2d6baadf88c13f0ab148c09b31f
Arty-Z <arty.zhuria@gmail.com>
2025-09-15 21:05:50 -0500

    Formated lines

--- arty/sauce_login_sad_path.spec.ts
+++ arty/sauce_login_sad_path.spec.ts
@@ -6,10 +6,10 @@ test('Positive sauce demo login functionality', async ({ page }) => {
     await page.locator('input[id="password"]').fill('secret_sauce')
     await page.locator('input[id="login-button"]').click()
 
-     let errMessage = await page.locator('h3[data-test="error"]')
-     let errMessTxt = await errMessage.innerText();
-    
-     let expectedErrMessage= "Epic sadface: Sorry, this user has been locked out.";
+    let errMessage = await page.locator('h3[data-test="error"]')
+    let errMessTxt = await errMessage.innerText();
 
-        expect(errMessage).toHaveText(expectedErrMessage)
+    let expectedErrMessage = "Epic sadface: Sorry, this user has been locked out.";
+
+    expect(errMessage).toHaveText(expectedErrMessage)
 });
