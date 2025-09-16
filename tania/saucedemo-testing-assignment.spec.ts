3af022835259404f281ea9ee2f107725348eab0a
thotska <thotska@gmail.com>
2025-09-15 20:40:53 -0500

    Formated Positive Log In Functionality Test

--- tania/saucedemo-testing-assignment.spec.ts
+++ tania/saucedemo-testing-assignment.spec.ts
@@ -1,19 +1,10 @@
 import { test, expect } from '@playwright/test'
-test("Positive login functionality test", async ({page}) => {
-
-// //Happy -> valid username, password
-
-
-// user goes to Swag Labs
-await page.goto("https://www.saucedemo.com/")
-// user enters valid username
-await page.locator('input[data-test="username"]').fill('standard_user')
-// user enters valid password.
-await page.locator('input[data-test="password"]').fill('secret_sauce')
-// user click on login button.
-await page.locator('input[class="submit-button btn_action"]').click()
-// user is redirected to their homepage.
-let expectedText = "Products"
- let titleLocator = page.locator('span[class="title"]');
-  await expect(titleLocator).toHaveText(expectedText)
+test("Positive login functionality test", async ({ page }) => {
+    await page.goto("https://www.saucedemo.com/")
+    await page.locator('input[data-test="username"]').fill('standard_user')
+    await page.locator('input[data-test="password"]').fill('secret_sauce')
+    await page.locator('input[class="submit-button btn_action"]').click()
+    let expectedText = "Products"
+    let titleLocator = page.locator('span[class="title"]');
+    await expect(titleLocator).toHaveText(expectedText)
 }) 
