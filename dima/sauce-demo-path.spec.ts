fe1a9d999068721a4ab5a5b42df1a8b3635f9716
freeeams <freams.fox@gmail.com>
2025-09-15 20:26:16 -0500

    Create sauce-demo-path.spec.ts

---
+++ dima/sauce-demo-path.spec.ts
@@ -0,0 +1,18 @@
+
+
+import { test, expect } from '@playwright/test'
+
+test('Positive sauce demo login functionality', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/')
+    await page.waitForTimeOut(5_000)
+    await page.locator('input[id="user-name"]').fill('standard_user')
+    await page.locator('input[id="password"]').fill('secret_sauce')
+    await page.waitForTimeOut(5_000)
+    await page.locator('input[id="login-button"]').click()
+    let expectedText = 'Products'
+    let productTitle = page.locator('span[class="title"]')
+    let actualText = await productTitle.innerText()
+    expect(productTitle).toHaveText(expectedText)
+})
+
+
