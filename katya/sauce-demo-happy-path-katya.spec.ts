184bbe06755fa6aa8ebe9766ce20cce85d1dfaac
kolesnikovak <katyamiller36@gmail.com>
2025-09-15 20:40:47 -0500

    Fixed code

--- katya/sauce-demo-happy-path-katya.spec.ts
+++ katya/sauce-demo-happy-path-katya.spec.ts
@@ -1,16 +1,13 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test('Happy path sauce demo page', async ({page}) => {
+test('Happy path sauce demo page', async ({ page }) => {
 
     await page.goto('https://www.saucedemo.com/')
     await page.locator('input[id="user-name"]').fill('standard_user')
     await page.locator('input[id="password"]').fill('secret_sauce')
     await page.locator('input[id="login-button"]').click()
 
-    let expectedTitleText = 'Products'
-    let actualTitleText = await page.locator('span[class="title"]').innerText()
-    expect(actualTitleText).toBe(expectedTitleText)
-    
-
-
+    let expectedTitle = 'Products'
+    let productTitle = page.locator('span[class="title"]')
+    expect(productTitle).toHaveText(expectedTitle)
 })  
