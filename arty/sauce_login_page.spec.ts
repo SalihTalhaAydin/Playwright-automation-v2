c7e7d1cdafb78d67227b2e9e37b21bd52a1a1c9a
Arty-Z <arty.zhuria@gmail.com>
2025-09-15 20:53:46 -0500

    Updated version
    toBe --->  toHaveText()

--- arty/sauce_login_page.spec.ts
+++ arty/sauce_login_page.spec.ts
@@ -1,24 +1,12 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
-
-test('Testing Sauce login page', async ({ page }) => {
-     
-     await page.goto('https://www.saucedemo.com/');
-     let loginInput = page.locator('div[class="form_group"] [id="user-name"]');
-     await loginInput.type("locked_out_user")
-     await page.waitForTimeout(2_000)
-
-     let passwordInput = page.locator('div[class="form_group"] [id="password"]');
-     await passwordInput.type("secret_sauce")
-     await page.waitForTimeout(2_000)
-
-     let loginbutton = page.locator('div[id="login_button_container"] [id="login-button"]');
-     await loginbutton.press("Enter");
-     await page.waitForTimeout(2_000)
-
-     let pageHeader = await page.locator('span[class="title"]').innerText();
-    
-     let expectedHeader= "Products";
-
-    expect (pageHeader).toBe(expectedHeader);
-});
+test('Positive sauce demo login functionality', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/')
+    await page.locator('input[id="user-name"]').fill('standard_user')
+    await page.locator('input[id="password"]').fill('secret_sauce')
+    await page.locator('input[id="login-button"]').click()
+    let expectedText = 'Products'
+    let productTitle = page.locator('span[class="title"]')
+    let actualText = await productTitle.innerText()
+    expect(productTitle).toHaveText(expectedText)
+})
