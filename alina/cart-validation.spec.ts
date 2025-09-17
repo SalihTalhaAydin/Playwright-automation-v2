450eb1f416e52da08b1cefdcf1d390a7c5e1024f
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-16 19:32:06 -0500

    created variables

--- alina/cart-validation.spec.ts
+++ alina/cart-validation.spec.ts
@@ -1,11 +1,17 @@
 import { test, expect } from "@playwright/test";
+let baseUrl = "https://www.saucedemo.com/";
+let userNameInputLocator = 'input[id="user-name"]';
+let passswordInputLocator = 'input[id="password"]';
+let loginButton = 'input[id="login-button"]';
+let cartIcon = 'a[class="shopping_cart_link"]';
+let cartTitleLocator = 'span[class="title"]';
 test("Cart functionality", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/");
-  await page.locator('input[id="user-name"]').fill("standard_user");
-  await page.locator('input[id="password"]').fill("secret_sauce");
-  await page.locator('input[id="login-button"]').click();
-  await page.locator('a[class="shopping_cart_link"]').click();
+  await page.goto(baseUrl);
+  await page.locator(userNameInputLocator).fill("standard_user");
+  await page.locator(passswordInputLocator).fill("secret_sauce");
+  await page.locator(loginButton).click();
+  await page.locator(cartIcon).click();
   let expectedText = "Your Cart";
-  let cartTitle = page.locator('span[class="title"]');
+  let cartTitle = page.locator(cartTitleLocator);
   expect(cartTitle).toHaveText(expectedText);
 });
